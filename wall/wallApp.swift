7d762a22c26c4c2f38a98730c91ae7c5f8b95994
fermin villanueva <FerVillanuev4s@gmail.com>
2026-02-28 13:07:25 -0700

    Initial Commit

---
+++ wall/wallApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallApp.swift
+//  wall
+//
+//  Created by Fermin on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct wallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
